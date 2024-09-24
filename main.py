aba04ae479f912b437c0d5d3faed05c940917725
boranj <egeboran2012@hotmail.com>
2024-09-24 15:25:45 +0100

    updated the main.py for book report

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-    print(count_words(text))
+    total_words = count_words(text)
+    characters = count_characters(text)
+    chars_sorted_list = chars_dict_to_sorted_list(characters)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{total_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list: 
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +33,15 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    chars = {}
+    for characters in text:
+        if characters.lower() not in chars:
+            chars[characters.lower()] = 1
+        chars[characters.lower()] += 1
+    return chars
+
+
 
 main()
